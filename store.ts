92de0b29c3c0fd6b4b20a3a944f9de39d853608d
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2024-12-20 17:40:45 -0500

    Update store.ts

--- store.ts
+++ store.ts
@@ -1,1 +1,14 @@
 
+import { configureStore } from '@reduxjs/toolkit';
+import collaborationReducer from './collaborationSlice';
+
+const store = configureStore({
+    reducer: {
+        collaboration: collaborationReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
